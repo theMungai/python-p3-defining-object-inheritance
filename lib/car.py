ac5450aa4a03702b82ed79d6f53a8ee6e7aec35d
theMungai <mungaithesimon@gmail.com>
2025-05-16 16:14:28 +0300

    object-inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
 from vehicle import Vehicle
 
+# Child class
 class Car(Vehicle):
-    pass
+    def __init__(self,wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
